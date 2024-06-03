fcfa734084a0ada92d71b72cdef3cde27bc49249
subh <harvirbuttar2917@gmail.com>
2024-06-03 13:04:08 -0700

    installed framer-motion + clsx & curently working on header

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,38 @@
 // Byimaan
+'use client';
 
-import React from 'react'
+import { motion } from 'framer-motion';
+import React from 'react';
+
+import {links} from "@/lib/data";
 
 function Header() {
-  return (
-    <div className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full transition -translate-x-1/2">
 
-    </div>
+  return (
+
+    <header className='relative z-[999]'>
+      <motion.div 
+        className="fixed top-0 left-[50%] translate-x-[-50%] h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full "
+        initial={
+          {
+            y: -100, opacity: 0, x: '-50%'
+          }
+        }
+        animate={
+          {
+            y: 0, opacity: 1, x: '-50%'
+          }
+        }
+      >
+
+      <nav className='flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0'>
+
+        <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5'>
+
+        </ul>
+      </nav>
+      </motion.div>
+    </header> 
   )
 }
 
