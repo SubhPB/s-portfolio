c25c059cc64c71a979310c84e321343cf5226fd2
subh <harvirbuttar2917@gmail.com>
2024-06-03 12:36:06 -0700

    initialized the home page with header and static files into the public dir

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import Header from "@/components/header";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,10 +20,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} relative bg-gray-50 text-zinc-950`}>
 
+        <Header/>
         <div className="bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"/>
         <div className="bg-[#dbd7fb] absolute -z-10 top-[-1rem] left-[-35rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem]"/>
-
         {children}
+
       </body>
     </html>
   );
---
+++ components/header.tsx
@@ -0,0 +1,13 @@
+// Byimaan
+
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full transition -translate-x-1/2">
+
+    </div>
+  )
+}
+
+export default Header
