9fa11db978c3774b3ccd83012a4bcd28f94f789f
Byimaan <byimaan1@gmail.com>
2024-06-03 23:30:17 -0700

    Intro page has been done!

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,10 @@ import Intro from "@/components/intro"
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
-      <Intro />
+
+      <div className="grid place-content-center h-dvh w-full">
+        <Intro />
+      </div>
     </main>
   )
 }
--- components/intro.tsx
+++ components/intro.tsx
@@ -6,6 +6,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
 
+
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
@@ -116,10 +117,46 @@ export default function Intro(){
             >
                 <Link
                     href='#contact'
-                    // need to start from here 80th line
-                    
+                    className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+                    onClick={
+                        () => {
+                            setActiveSection("Contact");
+                            setTimeOfLastClick(Date.now());
+                        }
+                    }
                 >
+                    Contact me here {" "}
+                    <BsArrowRight
+                        className="opacity-70 group-hover:translate-x-1 transition"
+                    />
                 </Link>
+
+                <a 
+                    href="/Resume.pdf"
+                    download={true}
+                    className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+                >
+                    Download Resume {" "}
+                    <HiDownload 
+                        className="opacity-60 group-hover:translate-y-1 transition"
+                    />
+                </a>
+
+                <a
+                className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+                href="https://linkedin.com"
+                target="_blank"
+                >
+                    <BsLinkedin />
+                </a>
+
+                <a
+                className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+                href="https://github.com"
+                target="_blank"
+                >
+                    <FaGithubSquare />
+                </a>
             </motion.div> 
         </section>
     )
