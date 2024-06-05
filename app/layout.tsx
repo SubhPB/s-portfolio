f0241074d551e36415daaabe207cd4e5bc960f10
subh <harvirbuttar2917@gmail.com>
2024-06-05 10:17:40 -0700

    injected theme provider in the layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 
 import Header from "@/components/header";
 import ActiveSectionContextProvider from "@/context/active-section-context";
+import ThemeContextProvider from "@/context/theme-context";
+import ThemeSwitch from "@/components/theme-switch";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,16 +20,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="!scroll-smooth">
       <body className={`${inter.className} relative bg-gray-50 text-zinc-950`}>
 
         <div className="bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"/>
         <div className="bg-[#dbd7fb] absolute -z-10 top-[-1rem] left-[-35rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem]"/>
 
-        <ActiveSectionContextProvider>
-          <Header/>
-          {children}
-        </ActiveSectionContextProvider>  
+        <ThemeContextProvider>
+          <ActiveSectionContextProvider>
+
+            <Header/>
+            {children}
+            <ThemeSwitch />
+
+          </ActiveSectionContextProvider>  
+        </ThemeContextProvider> 
 
       </body>
     </html>
