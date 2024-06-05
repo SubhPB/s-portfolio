5278f8e6c7218cc9b91afbf54e284fd7cd3d7d7c
subh <harvirbuttar2917@gmail.com>
2024-06-05 10:15:59 -0700

    added theme context & componenet to update the theme

---
+++ components/theme-switch.tsx
@@ -0,0 +1,23 @@
+// Byimaan
+
+'use client';
+
+import React from "react";
+import { useTheme } from "@/context/theme-context";
+import { BsMoon, BsSun } from "react-icons/bs";
+
+export default function ThemeSwitch(){
+
+    const {theme, toggleTheme} = useTheme();
+
+    return (
+        <button 
+        className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full grid place-content-center hover-scale-105 transition-all dark:bg-gray-950"
+        onClick={toggleTheme}
+        >
+            {
+                theme === 'light' ? <BsSun /> : <BsMoon />
+            }
+        </button>
+    )
+}
---
+++ context/theme-context.tsx
@@ -0,0 +1,69 @@
+// Byimaan
+
+'use client';
+
+import React, {useEffect, useState, createContext, useContext} from "react";
+import { isStringObject } from "util/types";
+
+const themes = ['light', 'dark'] as const;
+
+type Theme = (typeof themes)[number]
+
+type ThemeContextProviderProps = {
+    children: React.ReactNode;
+  };
+  
+type ThemeContextType = {
+    theme: Theme;
+    toggleTheme: () => void;
+};
+
+const ThemeContext = createContext<null | ThemeContextType>(null);
+
+export default function ThemeContextProvider({children}: ThemeContextProviderProps){
+
+    const [theme, setTheme] = useState<Theme>('light');
+
+    const toggleTheme = () => {
+        const newTheme = theme === 'light' ? 'dark' : 'light';
+        setTheme(newTheme);
+    };
+
+    useEffect(
+        () => {
+            const localTheme = localStorage.getItem('theme') as Theme | null;
+
+            if (localTheme && themes.includes(localTheme) && theme !== localTheme){
+                setTheme(localTheme)
+            }
+        }, []
+    );
+
+    useEffect(
+        () => {
+            const oppositeTheme = theme === 'light' ? 'dark': 'light';
+
+            const localTheme = localStorage.getItem('theme') as Theme | null;
+            const classList = document.documentElement.classList;
+
+            Array.from(classList).includes(oppositeTheme) && classList.remove(oppositeTheme);
+            !Array.from(classList).includes(theme) && classList.add(theme);
+            localTheme !== theme && localStorage.setItem('theme', theme);
+            
+        }, [theme]
+    );
+
+    return (
+        <ThemeContext.Provider value={{theme, toggleTheme}}>
+            {children}
+        </ThemeContext.Provider>    
+    )
+};
+
+export const useTheme = () => {
+    const context = useContext(ThemeContext);
+    if (!context){
+        throw new Error("useTheme must be used within a ThemeContextProvider")
+    };
+    return context;
+}
