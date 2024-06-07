83488968a45a55c3afd5fb3aa4062a5133c6beec
subh <harvirbuttar2917@gmail.com>
2024-06-06 17:48:22 -0700

    renamed

---
+++ components/journey.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import React from "react";
+
+import { VerticalTimeline as Timeline, VerticalTimelineElement as TimelineElem } from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
+
+import { journeyData } from "@/lib/data";
+import { useSectionInView } from "@/lib/hooks";
+
+import SectionHeading from "./section-heading";
+import { useTheme } from "@/context/theme-context";
+import { useInView } from "react-intersection-observer";
+
+
+
+function Journey() {
+
+    const {ref} = useSectionInView("Journey");
+    const {theme} = useTheme();
+    const isLight = theme === 'light';
+
+
+    return (
+        <section ref={ref} className="scroll-mt-28 mb-28 sm:mb-40 relative" id="journey">
+
+            <SectionHeading>My Journey</SectionHeading>
+
+            <Timeline lineColor='' animate={true}>
+                {
+                    journeyData.map(
+                        (exp, index) => {
+                            const {ref: childRef, inView} = useInView({
+                                triggerOnce: true
+                            })
+                            return (
+                                <div key={index} ref={childRef} className=" vertical-timeline-element ">
+
+                                    <TimelineElem
+                                        visible={inView}
+                                        contentStyle={
+                                            {
+                                                background: isLight ? '#f3f4f6' : 'rgba(255, 255, 255, 0.05)',
+                                                boxShadow: 'none',
+                                                border: '1px solid rgba(0, 0, 0, 0.5)',
+                                                textAlign: 'left',
+                                                padding: '1.3rem 2rem',
+                                            }
+                                        }
+                                        contentArrowStyle={
+                                            {
+                                                borderRight: isLight ? '0.4rem solid #9ca3af' : '0.4rem solid rgba(255, 255, 255, 0.5)',
+                                            }
+                                        }
+                                        
+                                        date={exp.date}
+                                        icon={exp.icon}
+                                        iconStyle={
+                                            {
+                                                background: isLight ? "white" :  "black",
+                                                fontSize: "1.5rem",
+                                                color: isLight ? "black" : "white",
+                                            }
+                                        }
+                                    >
+                                        <h3 className=" font-semibold capitalize">{exp.title}</h3>
+                                        <p className=" font-normal !mt-0">{exp.location}</p>
+                                        <p className="!mt-1 !font-normal text-gray-700 dark:text-gray-300">{exp.description}</p>
+                                        
+                                    </TimelineElem>    
+                                </div>
+                        )}
+                    )
+                }
+            </Timeline>   
+
+        </section>
+    )
+}
+
+export default Journey;
