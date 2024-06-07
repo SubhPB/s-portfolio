4265e17e39d8726a9851763b06eb1c50cfaea421
subh <harvirbuttar2917@gmail.com>
2024-06-07 11:01:53 -0700

    new chart favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import Footer from "@/components/footer";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import ThemeContextProvider from "@/context/theme-context";
 import ThemeSwitch from "@/components/theme-switch";
+import Whatsapp from "@/components/whatsapp";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,11 +22,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="!scroll-smooth">
-      <body className={`${inter.className} relative bg-gray-50 text-zinc-950`}>
+    <html lang="en" className="!scroll-smooth ">
+      <body className={`${inter.className} relative bg-gray-50 text-zinc-950 dark:text-gray-50 dark:text-opacity-50 dark:bg-zinc-900`}>
 
-        <div className="bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"/>
-        <div className="bg-[#dbd7fb] absolute -z-10 top-[-1rem] left-[-35rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem]"/>
+        <div className="bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"/>
+        <div className="bg-[#dbd7fb] absolute -z-10 top-[-1rem] left-[-35rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#676394]"/>
 
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
@@ -34,6 +35,7 @@ export default function RootLayout({
               {children}
             <Footer />
 
+            <Whatsapp />  
             <ThemeSwitch />
 
           </ActiveSectionContextProvider>  
--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@
 import Intro from "@/components/intro";
 import SectionDivider from "@/components/section-divider";
 import About from "@/components/about";
-import Projects from "@/components/projects";
-import Skills from "@/components/skills";
 import Experience from "@/components/experience";
+import Skills from "@/components/skills";
+import Journey from "@/components/journey";
 import Contact from "@/components/contact";
 
 export default function Home() {
@@ -20,11 +20,11 @@ export default function Home() {
       
       <About />
 
-      <Projects/>
+      <Experience />
 
       <Skills />
       
-      <Experience />
+      <Journey />
 
       <Contact />
 
